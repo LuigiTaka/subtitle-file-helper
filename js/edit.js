7782ce8dbceeed746e79535893d331fb0747f8b6
LuigiTaka <luigimendeszanchett@gmail.com>
2022-02-05 16:29:25 -0300

    feat: Adiciona operações de salvar uma legenda e baixar o arquivo completo

--- js/edit.js
+++ js/edit.js
@@ -1,3 +1,5 @@
+
+
 const renderListItem = (t,options) => {
     const $li = document.createElement("li");
     $li.classList.add("subtitle-list-item");
@@ -31,13 +33,23 @@ const getTracks = (id) => {
 const renderEditMode = (editData) => { 
     const $textarea = document.getElementById("subtitle-text");
     $textarea.innerText = editData.text;
+
+    $textarea.addEventListener('change',(e) => {
+        //dispara apenas quando o foco sai do elemento
+        let  text = e.target.value;
+        editData.text = text;
+    });
+
+    let $saveTrackTrigger = get("#save-track");
+    $saveTrackTrigger.addEventListener("click",(e) => { 
+        saveTrack( editData );
+    });
+
 }
+let params = getParams();
 
-const id = "0.6981381395160569";
-
-console.log("Render example: "+id);
-
-
+const id = params.id;
+const tracks = getTracks(id);
 const EditData = { 
     text:'',
     timestamp:{start:'',end:''},
@@ -46,21 +58,58 @@ const EditData = {
 };
 
 const onListItemClick = (e,track) => { 
-    console.info("on click handler");
-    console.log(track);
     let edit = Object.assign(EditData,track);
     renderEditMode(edit);
 
+
 }
 
-const tracks = getTracks(id);
-console.log(tracks);
+const $saveTrackTrigger = get("#save-track");
+const $downloadTrigger = get("#downloadBtn");
+
+const saveTrack = (track) => {
+    //carrega a legenda do localStorage... 
+    const target = tracks.find( x => x.order === track.order );
+    let updated = Object.assign(target,track);
+    updated = updateTrack(tracks,id);
+    renderList( tracks,renderConfig );
+}
+
+
+
+const downloadSubtitle = (id) => { 
+    //will load all subtitles from localStorage and bind then in one  single file.
+    let tracks = getTracks(id);
+    let parser = new SrtParser;
+    let content = parser.build( tracks );
+    downloadFile({
+        filename: 'subtitle.srt',
+        data: content,
+        type:'text',
+    })
+
+}
+
+$downloadTrigger.addEventListener('click',(e) => {
+    downloadSubtitle( id );
+})
+
+const renderList = (tracks,{ itemConfig,target }) => {
+    target.innerHTML = '';
+    tracks.forEach(element => {
+        let config = Object.assign({},itemConfig);
+        config.track = element;
+        let $el = renderListItem(element,config);
+        target.appendChild($el);
+    });
+}
+
 
 $list = document.getElementById("subtitle-list");
 
+const renderConfig = { 
+    itemConfig: {onClick: onListItemClick,track:null },
+    target:$list,
+}; 
 
-
-tracks.forEach(element => {
-    let $el = renderListItem(element,{ onClick: onListItemClick,track:element });
-    $list.appendChild($el);
-});
+renderList( tracks,renderConfig );
